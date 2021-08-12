0f3c52922fe84c6d730ff65646bbc4f4daef41d9
Emmanuel Ola <54866720+eoola@users.noreply.github.com>
2021-08-12 14:42:06 -0400

    adds collection view controller for lessons

---
+++ BankTree/LessonCategoryCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  LessonCategoryCollectionViewCell.swift
+//  BankTree
+//
+//  Created by Emmanuel Ola on 8/12/21.
+//
+
+import UIKit
+
+class LessonCategoryCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var label: UILabel!
+    
+}
